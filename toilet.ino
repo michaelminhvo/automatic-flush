83e082a2131e28dbee322cc11418f07b1b54d56a
Michael Vo <michaelvo@Michaels-MacBook-Pro.local>
2021-03-10 17:56:34 -0500

    update toilet

--- toilet.ino
+++ toilet.ino
@@ -2,125 +2,198 @@
 #include "NewPing.h"
 Servo servo;
 
-#define TRIGGER_PIN 2
-#define ECHO_PIN 3
+#define LED_OUTPUT 13
+#define TRIGGER_PIN 7
+#define ECHO_PIN 6
 #define SERVO_PIN 9
 #define MAX_DISTANCE 400
-#define QUICK_FLUSH_WINDOW_CM 10 // Holding hand close to the sensor flushes
-#define OCCUPANCY_WINDOW_CM 40 // Tweak this based on your toilet's length. This is where your body is with respect to the distance sensor.
+#define QUICK_FLUSH_WINDOW_START_CM 20 // Holding hand close to the sensor flushes
+#define OCCUPANCY_WINDOW_CM 80 // Tweak this based on your toilet's length. This is where your body is with respect to the distance sensor.
 #define MINIMUM_SENSITIVITY 2 // motion sensor cannot detect less than 2 centimeters away.
 #define ONE_SECOND_MS 1000
 #define FIVE_SECONDS_MS 5000
-#define THRESHOLD_SECONDS 3
+#define SEVEN_SECONDS_MS 7000
+#define RESET_WINDOW 5
+#define FLUSH_THRESHOLD_SECONDS 5
+#define TIMED_WINDOW 8
 #define TWO_SECONDS 2
 #define FIVE_SECONDS 5
-#define FLUSH_WAIT_TIME_SECONDS 10 
+#define FLUSH_WAIT_TIME_SECONDS 5
 #define INACTIVE_THRESHOLD_SECONDS 5
 #define TEN_MINUTES 600
 #define RESET_POSITION 0
-#define FLUSH_POSITION 90
+#define FLUSH_POSITION 100
+long FASTEST_BLINK = 100;
+long FAST_BLINK = 200;
+long SLOW_BLINK = 500;
+
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-void setup() {
-  Serial.begin(9600);
-  servo.attach(SERVO_PIN);
-  servo.write(0); 
-  delay(1000);
+void setIndicatorLED(int state) {
+  digitalWrite(LED_OUTPUT, state);
 }
 
-// Add a quick flush mode where if you wave the hand real close for 2 seconds it flushes
-bool isQuickFlush = false;
 
-bool isUsingToilet() {
-  float distance = 0;
-  int activeSeconds = 0;
-  int totalSeconds = 0;
-  int quickFlushSeconds = 0;
-  while(true) {
-    distance = sonar.ping_cm();
-    Serial.print("distance: ");
-    Serial.println(distance);
-    if (distance <= OCCUPANCY_WINDOW_CM && distance > MINIMUM_SENSITIVITY) {
-      activeSeconds = activeSeconds + 1;
+void setup() {
+  servo.attach(SERVO_PIN);
+  servo.write(0);
+  pinMode(LED_OUTPUT, OUTPUT);
+  setIndicatorLED(HIGH);
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Setup complete");
+}
+
+long blinkInterval = FAST_BLINK;
+bool shouldBlink = false;
+
+void setBlinkSpeed(long blinkSpeed) {
+  shouldBlink = true;
+  blinkInterval = blinkSpeed;
+}
+
+
+
+void blinkAndDelay(long blinkSpeed, long numMilliseconds) {
+  unsigned long previousMillis = millis();
+  unsigned long startMillis = millis();
+  int ledState = HIGH;
+  while (true) {
+    unsigned long currentMillis = millis();
+    long elapsedMillis = currentMillis - previousMillis;
+    if (elapsedMillis >= blinkSpeed) {
+      previousMillis = currentMillis;
+
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
+
+      setIndicatorLED(ledState);
     }
-    
-    if (distance <= QUICK_FLUSH_WINDOW_CM && distance > MINIMUM_SENSITIVITY) {
-      quickFlushSeconds = quickFlushSeconds + 1;
-    }
-    
-    totalSeconds = totalSeconds + 1;
-    delay(ONE_SECOND_MS);
-    
-    if (quickFlushSeconds >= TWO_SECONDS) {
-      Serial.println("Is quick flush");
-      isQuickFlush = true;
-      break;
-    }
-    if (totalSeconds >= FIVE_SECONDS) {
-      isQuickFlush = false;
+    if (currentMillis - startMillis >= numMilliseconds) {
       break;
     }
   }
-  
-  bool isInUse = activeSeconds >= THRESHOLD_SECONDS || isQuickFlush;
-  if (isInUse) {
-    Serial.print("Toilet is in use: ");
-    Serial.println(isInUse); 
+}
+
+void blinkAndDelay(long numMilliseconds) {
+  blinkAndDelay(blinkInterval, numMilliseconds);
+}
+
+bool isToiletSeatOccupied() {
+  setIndicatorLED(HIGH);
+
+  float distance = 0;
+  int totalSeconds = 0;
+  int threshholdSeconds = 0;
+
+  while (true) {
+    distance = sonar.ping_cm();
+    if (distance <= OCCUPANCY_WINDOW_CM && distance > QUICK_FLUSH_WINDOW_START_CM) {
+      threshholdSeconds = threshholdSeconds + 1;
+      totalSeconds = totalSeconds - 1;
+      setBlinkSpeed(FASTEST_BLINK);
+    } else {
+      threshholdSeconds - 1;
+      totalSeconds = totalSeconds + 1;
+    }
+
+    if (threshholdSeconds < 0) {
+      threshholdSeconds = 0;
+    }
+
+    if (totalSeconds < 0) {
+      totalSeconds = 0;
+    }
+
+    if (threshholdSeconds > 0) {
+      Serial.println(threshholdSeconds);
+      blinkAndDelay(ONE_SECOND_MS);
+    } else {
+      delay(ONE_SECOND_MS);
+      setIndicatorLED(HIGH);
+    }
+
+
+    if (totalSeconds >= RESET_WINDOW) {
+      totalSeconds = 0;
+      threshholdSeconds = 0;
+    }
+    
+    if (threshholdSeconds > FLUSH_THRESHOLD_SECONDS) {
+      Serial.println("Breaking out of loop");
+
+      break;
+    }
+    
+  }
+
+  bool isInUse = threshholdSeconds >= FLUSH_THRESHOLD_SECONDS;
+
+  if (!isInUse) {
+    setIndicatorLED(HIGHT);
   }
   
   return isInUse;
 }
 
-bool shouldFlush() {
-  Serial.print("Checking if we should flush");
-  if (isQuickFlush) {
-    Serial.print("Quick flush mode");
-    isQuickFlush = false;
-    return true;
-  }
+bool didPersonLeaveSeat() {
+  Serial.println("Waiting to flush");
+  setIndicatorLED(HIGH);
+
   float distance = 0;
   int activeSeconds = 0;
   int totalSeconds = 0;
-  while(true) {
+  while (true) {
     distance = sonar.ping_cm();
-    if (distance > OCCUPANCY_WINDOW_CM) {
-      activeSeconds = activeSeconds + 1;
-    } else if (distance <= OCCUPANCY_WINDOW_CM && distance > MINIMUM_SENSITIVITY) {
-      Serial.println("Resetting to 0");
+    if (distance <= OCCUPANCY_WINDOW_CM && distance > MINIMUM_SENSITIVITY) {
       activeSeconds = 0;
+    } else {
+      activeSeconds = activeSeconds + 1;
+      int remaining = FLUSH_WAIT_TIME_SECONDS - activeSeconds;
+      String flushText = String("Flushing in ") + remaining + "...";
     }
-    
-    totalSeconds = totalSeconds + 1;
-    delay(ONE_SECOND_MS);
-    Serial.print("No one using toilet for: ");
-    Serial.println(activeSeconds);
 
-    if (activeSeconds >= 10) {
-      Serial.print("Five seconds of inactivity");
+    totalSeconds = totalSeconds + 1;
+
+    if (activeSeconds > 0) {
+      Serial.println(activeSeconds);
+      blinkAndDelay(ONE_SECOND_MS);
+    } else {
+      setIndicatorLED(HIGH);
+      delay(ONE_SECOND_MS);
+    }
+
+    if (activeSeconds >= FLUSH_WAIT_TIME_SECONDS) {
       break;
     }
-    
+
     if (totalSeconds > TEN_MINUTES) {
-      Serial.print("Ok buddy it's been ten minutes I'm flushing");
       break;
     }
   }
-  
-  Serial.print("Nobody in front of sensor for:");
-  Serial.println(activeSeconds);
+
   return true;
 }
 
 void flush() {
-  Serial.println("Flushing...");
-  servo.write(FLUSH_POSITION);
-  delay(FIVE_SECONDS_MS);
+  Serial.println("Flushing. Reset position");
+  setIndicatorLED(LOW);
   servo.write(RESET_POSITION);
-  Serial.println("Done flushing");
+  delay(ONE_SECOND_MS);
+  setIndicatorLED(HIGH);
+  Serial.println("Turning servo arm");
+  servo.write(FLUSH_POSITION);
+  delay(SEVEN_SECONDS_MS);
+  setIndicatorLED(LOW);
+  servo.write(RESET_POSITION);
+  delay(FIVE_SECONDS_MS);
 }
 
 void loop() {
-  if (isUsingToilet() && shouldFlush()) {
+  if (isToiletSeatOccupied() && didPersonLeaveSeat()) {
     flush();
   }
 }
